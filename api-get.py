fe18fb0140a0d43e89d7564f5cba37da12fdd7f2
Aldertos <mentalitexd@gmail.com>
2025-04-12 02:57:53 +0300

    Update api-get.py

--- api-get.py
+++ api-get.py
@@ -1,3 +1,5 @@
+# invite api This works without token
+
 import requests
 
 vanity = input("Enter Vanity URL: ")
@@ -5,4 +7,23 @@ invite_api = f"https://discord.com/api/v9/invites/{vanity}"
 
 
 response = requests.get(invite_api)
+print(response.text)
+
+
+# API to get more detailed user information
+
+import requests
+
+user_id = input("Enter target ID: ")
+api_url = f"https://discord.com/api/v9/users/{user_id}/profile"
+token = "your account token"
+
+
+headers = {
+     "Authorization": token,
+}
+
+
+
+response = requests.get(api_url, headers=headers)
 print(response.text)
