ac34b4c131f89661a129c6729017c65692de06c0
Aldertos <mentalitexd@gmail.com>
2025-04-12 03:01:34 +0300

    Update api-get.py

--- api-get.py
+++ api-get.py
@@ -12,18 +12,3 @@ print(response.text)
 
 # API to get more detailed user information
 
-import requests
-
-user_id = input("Enter target ID: ")
-api_url = f"https://discord.com/api/v9/users/{user_id}/profile"
-token = "your account token"
-
-
-headers = {
-     "Authorization": token,
-}
-
-
-
-response = requests.get(api_url, headers=headers)
-print(response.text)
